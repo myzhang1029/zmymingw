b3f3a34cf15e1806ef20001efdf6c9678c5a6dde
Zhang Maiyun <me@maiyun.me>
2025-04-19 09:52:17 -0700

    Fix termcap install

--- zcbe/termcap.zcbe/build.sh
+++ zcbe/termcap.zcbe/build.sh
@@ -5,6 +5,7 @@ autoreconf -fi
 ./configure --host=${ZCHOST} --prefix=${ZCPREF}
 make CFLAGS=-fno-stack-protector\ -Wno-implicit-function-declaration\ -DSTDC_HEADERS
 make install
+install -d -m 755 "${ZCPREF}/etc/termcap"
 install -c -m 644 termcap.src "${ZCPREF}/etc/termcap"
 make maintainer-clean
 exit 0
