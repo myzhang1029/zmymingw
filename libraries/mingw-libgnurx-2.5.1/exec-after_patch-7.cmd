2897045584299eff05088cec605d3c241e3aba06
Zhang Maiyun <me@maiyun.me>
2025-04-19 20:41:44 -0700

    Convert help2man and libgnurx, delete libid3tag and libmad

--- libraries/mingw-libgnurx-2.5.1/exec-after_patch-7.cmd
+++
@@ -1,1 +0,0 @@
-automake --add-missing
--- zcbe/help2man.zcbe/build.sh
+++ zcbe/help2man.zcbe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+autoreconf -fi
+cp ../../libraries/gnulib/build-aux/mkinstalldirs build-aux || true
 ./configure --host="${ZCHOST}" --build="$("${ZCTOP}"/zcbe/config.guess)" --prefix="${ZCPREF}"
 make
 make install
