78c008acdfd8edeccbfbedb14bf87e49c9465fd5
Zhang Maiyun <me@maiyun.me>
2025-04-19 20:07:47 -0700

    Add vim

--- zcbe/vim.zcbe/build.sh
+++ zcbe/vim.zcbe/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-make -C src -f Make_cyg.mak CROSS_COMPILE=${ZCHOST}-
-mkdir -p ${ZCPREF}/opt/vim
-cp -r src/*.exe src/xxd/xxd.exe ${ZCPREF}/opt/vim
-cp -r runtime/* ${ZCPREF}/opt/vim
-make -C src -f Make_cyg.mak CROSS_COMPILE=${ZCHOST}- clean
+make -C src -f Make_ming.mak CROSS_COMPILE="${ZCHOST}-" CROSS=yes XPM=no WINDRES="${ZCHOST}-windres" VIMRUNTIMEDIR=../share/vim
+make -C src -f Make_ming.mak CROSS_COMPILE="${ZCHOST}-" CROSS=yes XPM=no WINDRES="${ZCHOST}-windres" GUI=no VIMRUNTIMEDIR=../share/vim
+install -d -m 755 "${ZCPREF}/bin" "${ZCPREF}/share/vim"
+cp -r src/vim.exe src/gvim.exe src/vimrun.exe src/xxd/xxd.exe "${ZCPREF}/bin"
+cp -r runtime/* "${ZCPREF}/share/vim"
+make -C src -f Make_ming.mak CROSS_COMPILE="${ZCHOST}-" CROSS=yes XPM=no WINDRES="${ZCHOST}-windres" GUI=no clean
+make -C src -f Make_ming.mak CROSS_COMPILE="${ZCHOST}-" CROSS=yes XPM=no WINDRES="${ZCHOST}-windres" clean
 exit 0
