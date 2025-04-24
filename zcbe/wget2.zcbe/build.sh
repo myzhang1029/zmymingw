b0014ad73d2caf96ee6b1def1c9978845a4ca302
Zhang Maiyun <me@maiyun.me>
2025-04-24 15:36:31 -0700

    Add working wget2

---
+++ zcbe/wget2.zcbe/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+./bootstrap --gnulib-srcdir="${ZCTOP}"/libraries/gnulib --no-git
+./configure --host="${ZCHOST}" --build="$("${ZCTOP}"/zcbe/config.guess)" --prefix="${ZCPREF}"
+make
+make install
+make distclean
+exit 0
