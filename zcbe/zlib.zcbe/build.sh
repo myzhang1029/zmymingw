1adfe34b9c82f6e65d6f15aec90843572ac9abd2
Zhang Maiyun <me@maiyun.me>
2025-04-24 19:12:02 -0700

    Fix clean for zlib

--- zcbe/zlib.zcbe/build.sh
+++ zcbe/zlib.zcbe/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 make -f win32/Makefile.gcc PREFIX=${ZCHOST}- DESTDIR=${ZCPREF} install prefix=${ZCPREF} LIBRARY_PATH=/lib INCLUDE_PATH=/include BINARY_PATH=/bin SHARED_MODE=1 CP=install
+make -f win32/Makefile.gcc clean
 exit 0
