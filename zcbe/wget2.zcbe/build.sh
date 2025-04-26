0ecbae345b3b2298c8c09dff8a99f84c22f75fcb
Zhang Maiyun <me@maiyun.me>
2025-04-26 11:29:56 -0700

    wget2 win32 patch

--- zcbe/wget2.zcbe/build.sh
+++ zcbe/wget2.zcbe/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+sed -i.zcbak 's,^static BOOL HandleCtrlEvent(DWORD dwCtrlType),static BOOL WINAPI HandleCtrlEvent(DWORD dwCtrlType),' src/wget.c
 ./bootstrap --gnulib-srcdir="${ZCTOP}"/libraries/gnulib --no-git
 ./configure --host="${ZCHOST}" --build="$("${ZCTOP}"/zcbe/config.guess)" --prefix="${ZCPREF}"
 make
 make install
 make distclean
+mv src/wget.c.zcbak src/wget.c
 exit 0
