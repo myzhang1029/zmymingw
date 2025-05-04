fc73b0ee7a34ee130a584b78cfa756cf451ca319
Zhang Maiyun <me@maiyun.me>
2025-05-03 20:23:15 -0700

    Absorb libressl, use cmake, and update to 4.1.0

--- zcbe/libressl.zcbe/build.sh
+++ zcbe/libressl.zcbe/build.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
-autoreconf -fi
-CFLAGS="-I${ZCPREF}/include" ./configure --host="${ZCHOST}" --build="$("${ZCTOP}"/zcbe/config.guess)" --prefix="${ZCPREF}" --enable-static --enable-shared
-make
-make install
-make distclean
+./autogen.sh
+"${ZCTOP}"/zcbe/gen_toolchainfile.sh
+ninja="$("${ZCTOP}"/zcbe/checkninja.sh)"
+case "${ZCHOST}" in
+    x86_64-*)
+        arch="HOST_X86_64"
+        ;;
+    aarch64-*)
+        arch="HOST_AARCH64"
+        ;;
+    i686-*)
+        arch="HOST_I386"
+        ;;
+    *)
+        echo "Unsupported architecture: ${ZCHOST}"
+        exit 1
+        ;;
+esac
+cmake -DCMAKE_TOOLCHAIN_FILE="${ZCPREF}"/tmp/toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${ZCPREF}" -DBUILD_SHARED_LIBS=ON "-D${arch}=yes" -G "${ninja}" -S . -B zcbe_build
+cmake --build zcbe_build
+cmake --install zcbe_build
+rm -rf zcbe_build
+
 exit 0
