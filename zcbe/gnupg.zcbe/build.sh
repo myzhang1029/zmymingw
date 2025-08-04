eec3c04efd9ff53ced9d18592c690cd40c3928e1
Zhang Maiyun <me@maiyun.me>
2025-08-04 21:32:58 +0900

    Update packages

--- zcbe/gnupg.zcbe/build.sh
+++ zcbe/gnupg.zcbe/build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-patch -p1 < "${ZCTOP}/zcbe/gnupg.zcbe/build.patch"
 ./autogen.sh
 ./configure --enable-maintainer-mode --prefix="${ZCPREF}" --host="${ZCHOST}" --build="$("${ZCTOP}"/zcbe/config.guess)" --with-libgpg-error-prefix=${ZCPREF} --with-ksba-prefix=${ZCPREF} --with-libgcrypt-prefix=${ZCPREF} --with-libassuan-prefix=${ZCPREF} --with-readline=${ZCPREF} --with-libiconv-prefix=${ZCPREF} --with-ntbtls-prefix=${ZCPREF} --with-npth-prefix=${ZCPREF} --with-libintl-prefix=${ZCPREF}
 make
 make install
 make distclean
-patch -R -p1 < "${ZCTOP}/zcbe/gnupg.zcbe/build.patch"
 exit 0
