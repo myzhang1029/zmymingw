d07e157a7433d61023e6e10cb7f0d18ec933f271
Zhang Maiyun <me@maiyun.me>
2025-11-03 08:19:58 -0500

    Patch bison for gnulib 427249c61666dcf649e6ba18fb327cdf71c45780

--- zcbe/bison.zcbe/build.sh
+++ zcbe/bison.zcbe/build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+patch -p1 < "${ZCTOP}/zcbe/bison.zcbe/build.patch"
+unpatch() {
+    patch -R -p1 < "${ZCTOP}/zcbe/bison.zcbe/build.patch"
+}
+trap unpatch exit
+
 test -d "${ZCTOP}/.git" && git submodule update --init submodules/autoconf
 ./bootstrap --gnulib-srcdir="${ZCTOP}"/libraries/gnulib --no-git
 ./configure --host="${ZCHOST}" --build="$("${ZCTOP}"/zcbe/config.guess)" --prefix="${ZCPREF}"
