c6ca86ae3372785b4c219046f1d5de4f18a3f9e6
Zhang Maiyun <me@maiyun.me>
2025-11-15 21:14:33 -0500

    More depend on libidn2

--- zcbe/libpsl.zcbe/build.sh
+++ zcbe/libpsl.zcbe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ./autogen.sh
-./configure --host="${ZCHOST}" --build="$("${ZCTOP}"/zcbe/config.guess)" --prefix="${ZCPREF}"
+./configure --host="${ZCHOST}" --build="$("${ZCTOP}"/zcbe/config.guess)" --prefix="${ZCPREF}" --with-libiconv-prefix="${ZCPREF}" --with-libintl-prefix="${ZCPREF}"
 make
 make install
 "${ZCTOP}/zcbe/strip_package_config.sh" libpsl
