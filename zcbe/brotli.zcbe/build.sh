0f7aa68aedbe54df8b8f76de8389d40f418c42de
Zhang Maiyun <me@maiyun.me>
2026-05-02 06:59:30 -0400

    Add brotli

---
+++ zcbe/brotli.zcbe/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+"${ZCTOP}"/zcbe/gen_toolchainfile.sh
+ninja="$("${ZCTOP}"/zcbe/checkninja.sh)"
+cmake -DCMAKE_TOOLCHAIN_FILE="${ZCPREF}"/tmp/toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${ZCPREF}" -G "${ninja}" -S . -B zcbe_build
+cmake --build zcbe_build
+cmake --install zcbe_build
+rm -rf zcbe_build
+
+exit 0
