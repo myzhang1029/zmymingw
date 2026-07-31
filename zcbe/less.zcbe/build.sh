c577f90d4c363fe7ddaf7e1b707f8d843bcce275
Zhang Maiyun <me@maiyun.me>
2026-07-31 13:44:45 -0400

    Make lessmsg.inc for less

--- zcbe/less.zcbe/build.sh
+++ zcbe/less.zcbe/build.sh
@@ -3,7 +3,7 @@
 autoreconf -fi
 LDFLAGS="-L${ZCPREF}/lib" CFLAGS="-isystem ${ZCPREF}/include" LIBS=-ltermcap ./configure --host="${ZCHOST}" --build="$("${ZCTOP}"/zcbe/config.guess)" --prefix="${ZCPREF}" --with-regex=pcre
 
-make -f Makefile.aut funcs.h less.nro lesskey.nro lessecho.nro
+make -f Makefile.aut lessmsg.inc funcs.h less.nro lesskey.nro lessecho.nro
 sed -i.zcbak "/MSDOS_COMPILER/d;" defines.h
 echo "#define MSDOS_COMPILER WIN32C">>defines.h
 echo "#define _MSC_VER 1910">>defines.h
