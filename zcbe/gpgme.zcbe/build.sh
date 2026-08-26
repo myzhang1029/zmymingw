46d13f882335bce222ae5bf62102e9eab3501243
Zhang Maiyun <me@maiyun.me>
2026-08-26 07:16:02 -0400

    Remove gpgme patch

--- zcbe/gpgme.zcbe/build.sh
+++ zcbe/gpgme.zcbe/build.sh
@@ -4,7 +4,7 @@ unpatch () {
     patch -R -p1 < "${ZCTOP}/zcbe/gpgme.zcbe/openssl.patch"
 }
 ./autogen.sh
-CFLAGS=-DDBL_EPSILON=__DBL_EPSILON__ ./configure --enable-maintainer-mode --prefix="${ZCPREF}" --host="${ZCHOST}" --build="$("${ZCTOP}"/zcbe/config.guess)" --with-libgpg-error-prefix="${ZCPREF}" --with-libassuan-prefix="${ZCPREF}"
+CC_FOR_BUILD=cc CFLAGS=-DDBL_EPSILON=__DBL_EPSILON__ ./configure --enable-maintainer-mode --prefix="${ZCPREF}" --host="${ZCHOST}" --build="$("${ZCTOP}"/zcbe/config.guess)" --with-libgpg-error-prefix="${ZCPREF}" --with-libassuan-prefix="${ZCPREF}"
 make
 make install
 "${ZCTOP}/zcbe/strip_package_config.sh" gpgme gpgme-glib
